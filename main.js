9a9242d4592df20bfc7cdc84a471dc9d488a9a88
Daniel Beristain <djberistain0@gmail.com>
2023-03-30 16:26:04 -0400

    new changes

--- main.js
+++ main.js
@@ -1,16 +1,4 @@
-// Define a function that will be called when the "Create User" button is clicked
-function createUser() {
-    // Prompt the user to enter their username
-    var username = prompt("Enter your username:");
-  
-    // If the user entered a username, display a message with their username
-    if (username) {
-      alert("Welcome to Jamspace, " + username + "!");
-    }
+function createUser() 
+  {
+    alert("Noice");
   }
-  
-  // Add a click event listener to the "Create User" button when the page is loaded
-  window.onload = function() {
-    document.getElementById("create-user-btn").addEventListener("click", createUser);
-  }
-  
